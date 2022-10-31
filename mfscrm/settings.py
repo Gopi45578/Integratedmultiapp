8dede8ec51ac358ec2abe367392341dc06aa33a0
Gopi Krishna <112347976+Gopi45578@users.noreply.github.com>
2022-10-30 20:16:10 -0500

    Update settings.py

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-j6)b=@)civ5dm!auv(t5w)fgea34-bc%pm2dkp)th_^#ld*-i1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 # Application definition
 
